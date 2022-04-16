b2f82773465141973d9bf52b336b626be362f8fe
dain <57316771+DainPark-web@users.noreply.github.com>
2022-04-16 13:24:15 +0200

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
+import { Helmet } from "react-helmet-async";
 
 
 const Home =() => {
     return (
+        <>
+        <Helmet>
+            <title>Home</title>
+        </Helmet>
         <div>
             Home
         </div>
+        </>
+        
     )
 }
 
