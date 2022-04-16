53e585a0cf143e81c151f1e65340a51ea94fcca2
dain <57316771+DainPark-web@users.noreply.github.com>
2022-04-16 15:39:04 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './components/App';
-
+import client from './apollo';
+import { ApolloProvider } from '@apollo/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-    <App />
-    </HelmetProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,54 @@
 import { Helmet } from "react-helmet-async";
+import { gql, useQuery } from "@apollo/client";
+
+
+const GET_MOVIES = gql`
+ { 
+    movies{
+    id
+    title
+    background_image
+  }
+  }
+`
 
 
 const Home =() => {
-    return (
-        <>
-        <Helmet>
-            <title>Home</title>
-        </Helmet>
-        <div>
-            Home
-        </div>
-        </>
-        
-    )
+    const {loading, error, data} =useQuery(GET_MOVIES);
+    console.log(loading, error, data);
+
+    if(loading){
+        return (
+            <div>
+            <Helmet>
+                <title>Loading...</title>
+            </Helmet>
+            <div>Loading...</div>
+            </div>
+            )
+    }
+
+
+    if(!loading){
+        return (
+            <>
+            <Helmet>
+                <title>Home</title>
+            </Helmet>
+            {
+                data.movies.map((e,i) => {
+                    return (
+                        <div key={i}>{e.title}</div>
+                    )
+                })
+            }
+            </>
+            
+        )
+
+    }
+
+    
 }
 
 export default Home;
